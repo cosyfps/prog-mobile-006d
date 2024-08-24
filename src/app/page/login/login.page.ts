16829c117d5f5c58dfff1c12c419a3a90002ba88
cosy <kelvin.morenog28@gmail.com>
2024-08-24 15:29:13 -0400

    Create. Creacion de /login.page.html y navegacion

--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  Email:String ='';
+  Password:String ='';
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.router.navigate(['/today']);
+  }
+
+  forgotPassword() {
+    this.router.navigate(['']);
+  }
+
+  signIn() {
+    this.router.navigate(['/register']);
   }
 
 }
