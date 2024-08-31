22f38478ff7e88819963aebb404d6aa69f0a22ca
cosy <kelvin.morenog28@gmail.com>
2024-08-31 03:46:51 -0400

    Update. creacion de pagina error404

--- src/app/pages/not-found404/not-found404.page.ts
+++ src/app/pages/not-found404/not-found404.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-not-found404',
   templateUrl: './not-found404.page.html',
   styleUrls: ['./not-found404.page.scss'],
 })
-export class NotFound404Page implements OnInit {
+export class NotFound404Page {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  goToLogin() {
+    this.navCtrl.navigateRoot('/login');  
   }
 
 }
