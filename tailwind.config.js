2c61ff50b4c90473916af63a11a61ed87495bb74
b3hidalgo <be.hidalgog@duocuc.cl>
2024-08-31 23:43:46 -0400

    tarjetas de actividad, ejemplos:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Agrega esta línea
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
